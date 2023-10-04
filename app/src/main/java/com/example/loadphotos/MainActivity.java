77ce8439fe82a3859a2e60ab8f2c68eedc0b649e
Omart <omarham206@gmail.com>
2023-10-04 18:21:29 +0300

    Completed the project

--- app/src/main/java/com/example/loadphotos/MainActivity.java
+++ app/src/main/java/com/example/loadphotos/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.loadphotos;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            ImageView imageView = findViewById(R.id.imgGlide);
+            imageView.setImageURI(selectedImage);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
