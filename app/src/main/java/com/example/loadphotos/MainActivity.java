0fb574357205448efe25e8a65fe5fd309c745573
OmarH260 <144048142+OmarH260@users.noreply.github.com>
2023-10-04 18:01:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/loadphotos/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.loadphotos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button gallery = findViewById(R.id.gallery);
+
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 3);
+
+            }
+        });
+        
+    }
+}
